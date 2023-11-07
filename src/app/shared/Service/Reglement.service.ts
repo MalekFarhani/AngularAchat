8bc0c92e2b5ba21050c965c1831372f13edf2e1b
MalekFarhani <92673668+MalekFarhani@users.noreply.github.com>
2023-11-07 23:05:12 +0100

    xs

--- src/app/shared/Service/Reglement.service.ts
+++ src/app/shared/Service/Reglement.service.ts
@@ -6,7 +6,7 @@ import {HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class ReglementService {
-  readonly API_URL = 'http://192.168.49.2:32692/SpringMVC/reglement';
+  readonly API_URL = 'https://back.internal.purplesmoke-9f3a9279.westeurope.azurecontainerapps.io/SpringMVC/reglement';
 
   constructor(private httpClient: HttpClient) {
   }
