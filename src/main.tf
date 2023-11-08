567f2b96bc8f1f0c50b875ed41726097c764bd1b
MalekFarhani <92673668+MalekFarhani@users.noreply.github.com>
2023-11-08 23:06:44 +0100

    oneshot

---
+++ src/main.tf
@@ -0,0 +1,34 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "prod" {
+  name     = "maRessourceGroup"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "prodk8s" {
+  name                = "maAKSCluster"
+  location            = azurerm_resource_group.prod.location
+  resource_group_name = azurerm_resource_group.prod.name
+  dns_prefix          = "myaksdns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.prodk8s.kube_config_raw
+  sensitive = true
+}
+
+output "cluster_name" {
+  value = azurerm_kubernetes_cluster.prodk8s.name
+}
---
+++ src/setup-aks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Provision the AKS cluster with Terraform
+terraform init
+terraform apply -auto-approve
+
+# Export the KUBECONFIG environment variable
+export KUBECONFIG=$(terraform output kube_config)
+
+# Display the AKS cluster name
+echo "AKS Cluster Name: $(terraform output cluster_name)"
