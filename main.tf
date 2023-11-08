373d40ecf1340503a95e0b1831ef34d06cf72973
MalekFarhani <92673668+MalekFarhani@users.noreply.github.com>
2023-11-09 00:10:50 +0100

    mlk

--- main.tf
+++ main.tf
@@ -3,12 +3,12 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "prod" {
-  name     = "maRessourceGroup"
+  name     = "malekRessourceGroup"
   location = "East US"
 }
 
 resource "azurerm_kubernetes_cluster" "prodk8s" {
-  name                = "maAKSCluster"
+  name                = "malekAKSCluster"
   location            = azurerm_resource_group.prod.location
   resource_group_name = azurerm_resource_group.prod.name
   dns_prefix          = "myaksdns"
